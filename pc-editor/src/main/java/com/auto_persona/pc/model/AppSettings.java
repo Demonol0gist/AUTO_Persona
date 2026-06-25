8586aa5141ad5dc2e1c5998939e35d1c4be40bd8
Demonologist <1392521073@qq.com>
2026-06-25 22:33:00 +0800

    合并 PC 端编辑器到主分支

---
+++ pc-editor/src/main/java/com/auto_persona/pc/model/AppSettings.java
@@ -0,0 +1,13 @@
+package com.auto_persona.pc.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppSettings {
+    public int bgmVolume = 70;
+    public int voiceVolume = 80;
+    public boolean isMuted;
+    public Map<String, String> longTermMemory = new HashMap<>();
+    public boolean ttsEnabled = true;
+    public boolean presetRepliesEnabled = true;
+}
